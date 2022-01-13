5d4a813bb1c619ea74e5ab168d0ea74baafa95d4
Yuan Shuai <shuai.yuan@biomind.ai>
2022-01-13 17:08:16 +0800

    Savepoint 1.

---
+++ vm.h
@@ -0,0 +1,35 @@
+//
+// Created by Biomind on 2022/1/13.
+//
+
+#ifndef CLOX_VM_H
+#define CLOX_VM_H
+
+#include "chunk.h"
+
+#define STACK_MAX 256
+
+typedef struct {
+    Chunk *chunk;
+    uint8_t *ip;
+    Value stack[STACK_MAX];
+    Value *stackTop;
+} VM;
+
+typedef enum {
+    INTERPRET_OK,
+    INTERPRET_COMPILE_ERROR,
+    INTERPRET_RUNTIME_ERROR
+} InterpretResult;
+
+void push(Value value);
+
+Value pop();
+
+void initVM();
+
+void freeVM();
+
+InterpretResult interpret(Chunk *chunk);
+
+#endif //CLOX_VM_H
